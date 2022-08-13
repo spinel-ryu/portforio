b25fa2d28bb4f1c20232103b539733d1ecd3ed25
spinel-ryu <68238148+spinel-ryu@users.noreply.github.com>
2022-08-13 17:09:10 +0900

    Add files via upload

--- move.js
+++ move.js
@@ -1,4 +1,4 @@
-$('#page-link a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+$('#page-link1 a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
 	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
 	var pos = $(elmHash).offset().top;	//idの上部の距離を取得
 	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
