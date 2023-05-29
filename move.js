c107f10bc4edbe42e03673060483cd185179b8e7
spinel-ryu <68238148+spinel-ryu@users.noreply.github.com>
2023-05-29 15:17:06 +0900

    Add files via upload

--- move.js
+++ move.js
@@ -1,6 +1,9 @@
-$('#page-link1 a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
-	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-	var pos = $(elmHash).offset().top;	//idの上部の距離を取得
-	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
-	return false;
-});
+$('#page-link1 a[href*="#"]').click(function (event) {
+	event.preventDefault();
+	
+	var elmHash = $(this).attr('href');
+	var pos = $(elmHash).offset().top;
+	
+	$('html').animate({scrollTop: pos}, 500);
+  });
+  
